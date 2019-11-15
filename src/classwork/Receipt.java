5f1aaa1cd803496416a68300c8e9bdd1b3a3a0ef
Dmitry <dmitry.ivoninsky@gmail.com>
2019-11-15 18:33:04 +0300

    intermediate commit

--- src/classwork/Receipt.java
+++
@@ -1,6 +0,0 @@
-package classwork;
-
-public class Receipt {
-
-
-}
--- src/classwork/Shop.java
+++ src/classwork/Shop.java
@@ -6,51 +6,53 @@ import java.util.List;
 import java.util.Map;
 
 public class Shop {
-    private List<Item> list = new ArrayList<>();
+    private List<Item> listOfItems = new ArrayList<>();
 
 
     public void putInShop (Item item) {
-        list.add(item);
+        listOfItems.add(item);
     }
 
-    public List<Item> getList() {
-        return list;
+    public List<Item> getListOfItems() {
+        return listOfItems;
     }
 
     public List<Integer> getListWithIdItem (){
         List<Integer> listIds = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            listIds.add(list.get(i).getId());
+        for (int i = 0; i < listOfItems.size(); i++) {
+            listIds.add(listOfItems.get(i).getId());
         }
         return listIds;
     }
 
 
     public Map getCheck(List<Integer> listIds) {
-        Map<Item, Integer> map = new HashMap();
+        Map<String, Integer> map = new HashMap();
         int count;
+        int index;
         for (int i = 0; i < listIds.size(); i++) {
             count = 0;
-            for (int j = 0; j < list.size(); j++) {
-                if (list.get(j).getId()) {
+            index = 0;
+            for (int j = 0; j < listOfItems.size(); j++) {
+                if (listIds.get(i) == listOfItems.get(j).getId()) {
                     count++;
+                    index = j;
                 }
             }
-            map.put(list.get(i), count);
+            map.put(listOfItems.get(index).getName(), count);
         }
         return map;
     }
 
     public int getTotalPrice() {
         int sum = 0;
-        for (int i = 0; i < list.size(); i++) {
-            sum += list.get(i).getPrice();
+        for (int i = 0; i < listOfItems.size(); i++) {
+            sum += listOfItems.get(i).getPrice();
         }
         return sum;
     }
 
     public void printCheck () {
-
         Map map = getCheck();
         System.out.println(map);
         System.out.println(getTotalPrice());
